c8e9499b161a2890643557f15a6f6d80e3953d76
Cory J. Cascalheira <corycaaz@gmail.com>
2021-06-22 09:18:32 -0600

    Fix query

--- src/get_tweets.R
+++ src/get_tweets.R
@@ -7,7 +7,7 @@ bearer_token <- Sys.getenv("TWITTER_ACADEMIC_BEARER")
 
 # Build query
 trans_query <- build_query(
-  query = "anti-trans OR anti-transgender OR (trans sports) OR (trans ban)",
+  query = 'trans sports',
   is_retweet = FALSE,
   lang = "en"
 )
@@ -17,7 +17,7 @@ tweets0 <- get_all_tweets(query = trans_query,
                           start_tweets = "2021-03-11T00:00:00Z", # start date 
                           end_tweets = "2021-04-10T23:59:59Z", # end date
                           bearer_token = bearer_token, # bearer token to access Twitter API 
-                          file = "trans_sports_ban0" # name of the resulting RDS file
+                          file = "data/trans_sports_ban0" # name of the resulting RDS file
 )
 
 # Pull tweets - April 11th - May 10th 
@@ -25,7 +25,7 @@ tweets1 <- get_all_tweets(query = trans_query,
                           start_tweets = "2021-04-11T00:00:00Z", # start date 
                           end_tweets = "2021-05-10T23:59:59Z", # end date
                           bearer_token = bearer_token, # bearer token to access Twitter API 
-                          file = "trans_sports_ban1" # name of the resulting RDS file
+                          file = "data/trans_sports_ban1" # name of the resulting RDS file
 )
 
 # Pull tweets - May 11th - June 10th
@@ -33,7 +33,7 @@ tweets2 <- get_all_tweets(query = trans_query,
                           start_tweets = "2021-05-11T00:00:00Z", # start date 
                           end_tweets = "2021-06-10T23:59:59Z", # end date
                           bearer_token = bearer_token, # bearer token to access Twitter API 
-                          file = "trans_sports_ban2" # name of the resulting RDS file
+                          file = "data/trans_sports_ban2" # name of the resulting RDS file
 )
 
 # Pull tweets - June 11th - June 22nd
@@ -41,7 +41,7 @@ tweets3 <- get_all_tweets(query = trans_query,
                           start_tweets = "2021-06-11T00:00:00Z", # start date 
                           end_tweets = "2021-06-22T00:00:00Z", # end date
                           bearer_token = bearer_token, # bearer token to access Twitter API 
-                          file = "trans_sports_ban3" # name of the resulting RDS file
+                          file = "data/trans_sports_ban3" # name of the resulting RDS file
 )
 
 ## You will see a warning, don't worry! It is just warning us about data loss when we work with large amounts of 
