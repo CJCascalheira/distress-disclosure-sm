f46de18bb3a73db123c3845a942444b159ac7dea
Cory J. Cascalheira <corycaaz@gmail.com>
2021-06-22 14:33:13 -0600

    Finish pre-processing

--- src/preprocess_unnest.R
+++ src/preprocess_unnest.R
@@ -28,6 +28,8 @@ table(trans_sports_full$lang)
 
 # Select relevant columns
 trans_sports_full1 <- trans_sports_full %>%
+  mutate(text_original = text) %>%
+  select(author_id, created_at, text, everything()) %>%
   mutate(created_at = ymd_hms(created_at)) %>%
   # Remove Korean words
   filter(author_id != trans_sports_full[22488, ]$author_id) %>%
@@ -38,15 +40,19 @@ trans_sports_full1 <- trans_sports_full %>%
     text = str_replace_all(text, "[:space:]{2,}", " ")
   ) %>%
   # Remove punctuation
-  mutate(text = str_replace_all(text, "[[:punct:]]", "")) %>%
-  mutate(text = str_replace_all(text, "[0-9]+", "")) %>%
+  mutate(text = str_replace_all(text, "[[:punct:]]", " ")) %>%
+  mutate(text = str_replace_all(text, "[0-9]+", " ")) %>%
   # Remove odd characters
-  mutate(text = str_replace_all(text, "[\\W+]", "")) %>%
+  mutate(text = str_replace_all(text, "[\\W+]", " ")) %>%
   unnest_tokens(word, text) %>%
-  select(author_id, created_at, word, everything()) %>%
   # Remove stop words
   anti_join(stop_words) %>%
   anti_join(custom_stop_words) %>%
   # Reduce to word stem
-  mutate(word = wordStem(word, language = "en"))
+  mutate(word = wordStem(word, language = "en")) %>%
+  # Move main variables to the front
+  select(author_id, created_at, word, everything())
 trans_sports_full1
+
+# Save to file
+saveRDS(trans_sports_full1, file = "data/trans_sports_clean.rds")
