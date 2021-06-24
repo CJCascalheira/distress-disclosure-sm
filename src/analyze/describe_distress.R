bd9fea77ade21ea5eba174c2f23c4c5c28227b15
Cory J. Cascalheira <corycaaz@gmail.com>
2021-06-24 15:03:49 -0600

    Update distress tests

--- src/analyze/describe_distress.R
+++ src/analyze/describe_distress.R
@@ -8,7 +8,7 @@ trans_distress_v2
 # Total count
 nrow(trans_distress_v2)
 
-# COMPARE -----------------------------------------------------------------
+# BASIC COMPARISON --------------------------------------------------------
 
 # Basic count - uncorrected
 trans_distress_v2 %>%
@@ -34,3 +34,60 @@ trans_distress_v2 %>%
     percent = n / sum
   )
 
+# DESCRIBE THE GROUPS -----------------------------------------------------
+
+# Summary variables
+trans_distress_v2 %>%
+  group_by(distress_tweet) %>%
+  summarize(
+    analytic = mean(Analytic),
+    clout = mean(Clout),
+    authentic = mean(Authentic),
+    tone = mean(Tone)
+  )
+
+# Cognitive features
+trans_distress_v2 %>%
+  group_by(distress_tweet) %>%
+  summarize(
+    # swear = mean(swear),
+    focus_future = mean(focusfuture),
+    focus_present = mean(focuspresent),
+    discrep = mean(discrep),
+    certain = mean(certain),
+    insight = mean(insight)
+  )
+
+# Separate data sets
+distress_no <- trans_distress_v2 %>%
+  filter(distress_tweet == 0)
+
+distress_yes <- trans_distress_v2 %>%
+  filter(distress_tweet == 1)
+
+# Running t tests
+t.test(distress_no$Analytic, distress_yes$Analytic)
+t.test(distress_no$Clout, distress_yes$Clout)
+t.test(distress_no$Authentic, distress_yes$Authentic)
+t.test(distress_no$Tone, distress_yes$Tone)
+t.test(distress_no$focusfuture, distress_yes$focusfuture)
+t.test(distress_no$focuspresent, distress_yes$focuspresent)
+t.test(distress_no$discrep, distress_yes$discrep)
+t.test(distress_no$certain, distress_yes$certain)
+t.test(distress_no$insight, distress_yes$insight)
+
+# TOP WORDS ---------------------------------------------------------------
+
+top_words %>%
+  slice(1:20) %>%
+  ggplot(aes(x=reorder(word, -n), y=n, fill=word))+
+  geom_bar(stat="identity")+
+  theme_minimal()+
+  theme(axis.text.x = 
+          element_text(angle = 60, hjust = 1, size=13))+
+  theme(plot.title = 
+          element_text(hjust = 0.5, size=18))+
+  ylab("Frequency")+
+  xlab("")+
+  ggtitle("Most Frequent Words in anti-trans sports ban Tweets")+  
+  guides(fill=FALSE)
