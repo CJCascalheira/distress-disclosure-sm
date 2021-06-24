5c78bad810f4634767259eb5b6d949df3ab94ed7
Cory J. Cascalheira <corycaaz@gmail.com>
2021-06-24 13:12:43 -0600

    Finish manual coding of distress tweet v2

---
+++ src/analyze/describe_distress.R
@@ -0,0 +1,10 @@
+# Dependencies
+library(tidyverse)
+
+# Import
+trans_distress_v2 <- read_csv("data/distress_tweets/trans_distress_v2_manual.csv")
+trans_distress_v2
+
+# COMPARE -----------------------------------------------------------------
+
+
